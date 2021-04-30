0d1605da454d91daa4957c807bfd2401002f168b
alye <yuyuye958@qq.com>
2021-04-30 18:20:50 +0800

    Switch组件 使用v-model

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Switch</div>
+  <div>
+    <Switch v-model:value="bool"/>
+  </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'SwitchDemo',
+  components: { Switch },
+  setup() {
+    const bool = ref(false)
+    return { bool }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <h1>Tracer UI</h1>
       <h2>一个基于 Vue3 的 UI 框架</h2>
       <p class="actions">
-        <a href="https://github.com">GitHub</a>
+        <a href="https://github.com/yuyuye958/Tracer-UI">GitHub</a>
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
