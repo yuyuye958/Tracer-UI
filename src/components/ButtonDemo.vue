926230be8867d0992868a7fd92a82886c45ac20c
alye <yuyuye958@qq.com>
2021-05-06 18:47:22 +0800

    初始化 Button 组件 & 优化 class 命名

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>Button</div>
+  <div>Button 示例</div>
+  <div>
+    <Button @click="onClick">Button1</Button>
+    <Button theme="button" @click="onClick">Button2</Button>
+    <Button theme="link" @click="onClick">Button3</Button>
+    <Button theme="text" @click="onClick">Button4</Button>
+  </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+
+export default {
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log('test')
+    }
+    return { onClick }
+  }
+};
+</script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{checked: value}">
+  <button class="t-switch" @click="toggle" :class="{'t-checked': value}">
     <span/>
   </button>
 </template>
@@ -19,10 +19,10 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.t-switch {
   height: $h;
   width: $h * 2;
   border: none;
@@ -41,7 +41,7 @@ button {
     transition: all 250ms;
   }
 
-  &.checked {
+  &.t-checked {
     background: #1890ff;
 
     > span {
@@ -59,7 +59,7 @@ button {
     }
   }
 
-  &.checked:active {
+  &.t-checked:active {
     > span {
       width: $h2 + 4px;
       margin-left: -4px;
